ca3cc4477b7c481f451c1222bd81cf10ee91be47
lakhdari_abdallah <110565989+abdallahlakh@users.noreply.github.com>
2024-02-21 23:06:20 +0100

    Update AdvancedSearch.js

--- src/AdvancedSearch.js
+++ src/AdvancedSearch.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import HeaderSection from './HeaderSection ';
+
+import HeaderSection from './HeaderSection';
 import Images from './Images';
 import FilterSection from './FilterSection';
+import Navbar from './Navbar';
 const AdvancedSearch = () => {
     return (
         <div>
+            <Navbar/>
             <HeaderSection />
             <Images />
             <FilterSection />
             
-           
         </div>
     );
 };
